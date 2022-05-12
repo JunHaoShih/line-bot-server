c68493e1b64ba5afaba92e8ddfa60a99e2fdc19c
JunHaoShih <victor5517@gmail.com>
2022-05-12 21:56:20 +0800

    移除多餘import

--- src/main/java/io/github/junhaoshih/linebotserver/LineBotServerApplication.java
+++ src/main/java/io/github/junhaoshih/linebotserver/LineBotServerApplication.java
@@ -1,31 +1,7 @@
 package io.github.junhaoshih.linebotserver;
 
-import com.linecorp.bot.model.action.Action;
-import com.linecorp.bot.model.action.URIAction;
-import com.linecorp.bot.model.event.Event;
-import com.linecorp.bot.model.event.MessageEvent;
-import com.linecorp.bot.model.event.message.TextMessageContent;
-import com.linecorp.bot.model.message.FlexMessage;
-import com.linecorp.bot.model.message.Message;
-import com.linecorp.bot.model.message.TextMessage;
-import com.linecorp.bot.model.message.flex.component.*;
-import com.linecorp.bot.model.message.flex.container.Bubble;
-import com.linecorp.bot.model.message.flex.unit.FlexAlign;
-import com.linecorp.bot.model.message.flex.unit.FlexLayout;
-import com.linecorp.bot.spring.boot.annotation.EventMapping;
-import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
-import io.github.junhaoshih.linebotserver.service.CommandService;
-import io.github.junhaoshih.linebotserver.service.DiceService;
-import io.github.junhaoshih.linebotserver.service.InvoiceService;
-import io.github.junhaoshih.linebotserver.service.cpc.CPCOilPriceService;
-import io.github.junhaoshih.linebotserver.service.cwb.WeatherService;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.net.URI;
-import java.util.*;
 
 @SpringBootApplication
 public class LineBotServerApplication {
