a9255df2339060019b6eced0a7373f7b29d17a8f
JunHaoShih <victor5517@gmail.com>
2022-12-04 21:31:28 +0800

    fix: Add JsonIgnoreProperties to prevent json change

--- src/main/java/io/github/junhaoshih/linebotserver/data/cpc/CpcOilPrice.java
+++ src/main/java/io/github/junhaoshih/linebotserver/data/cpc/CpcOilPrice.java
@@ -2,6 +2,8 @@ package io.github.junhaoshih.linebotserver.data.cpc;
 
 import java.math.BigDecimal;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,6 +14,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class CpcOilPrice {
 	
 	/**
@@ -23,4 +26,10 @@ public class CpcOilPrice {
 	 * 價格
 	 */
 	private BigDecimal y;
+
+	/**
+	 * Group id
+	 */
+	@JsonProperty("GroupID")
+	private int groupId;
 }
